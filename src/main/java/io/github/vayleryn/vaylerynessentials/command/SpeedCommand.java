028d63b1452be5f390f72c82f84308490ec20890
Lucariatias <lucariatias@gmail.com>
2013-10-14 23:40:33 +0100

    Change speed command to use floats

--- src/main/java/io/github/vayleryn/vaylerynessentials/command/SpeedCommand.java
+++ src/main/java/io/github/vayleryn/vaylerynessentials/command/SpeedCommand.java
@@ -23,25 +23,29 @@ public class SpeedCommand implements CommandExecutor {
 			if (sender instanceof Player) {
 				player = (Player) sender;
 			}
-			int speed = 0;
+			float speed = 0;
 			if (args.length >= 2 && plugin.getServer().getPlayer(args[0]) != null) {
 				player = plugin.getServer().getPlayer(args[0]);
 				try {
-					speed = Integer.parseInt(args[1]);
+					speed = Float.parseFloat(args[1]);
 				} catch (NumberFormatException exception) {
-					sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "Speed must be an integer."); 
+					sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "Speed must be a number."); 
 				}
 			} else {
 				try {
-					speed = Integer.parseInt(args[0]);
+					speed = Float.parseFloat(args[0]);
 				} catch (NumberFormatException exception) {
-					sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "Speed must be an integer.");
+					sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "Speed must be a number.");
 				}
 			}
 			if (player != null) {
-				player.setFlySpeed(speed);
-				sender.sendMessage(plugin.getPrefix() + ChatColor.GREEN + "Set " + player.getName() + "'s fly speed to " + speed);
-				player.sendMessage(plugin.getPrefix() + ChatColor.GREEN + "Your flyspeed was set to " + speed + " by " + sender.getName());
+				if (speed >= 0 && speed <= 1) {
+					player.setFlySpeed(speed);
+					sender.sendMessage(plugin.getPrefix() + ChatColor.GREEN + "Set " + player.getName() + "'s fly speed to " + speed);
+					player.sendMessage(plugin.getPrefix() + ChatColor.GREEN + "Your flyspeed was set to " + speed + " by " + sender.getName());
+				} else {
+					sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "Speed must be between 0 and 1");
+				}
 			} else {
 				sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "You must specify a player if you're using this command from console.");
 			}
